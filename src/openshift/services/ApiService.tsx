484f235e969e53b67f132f6beb086e8bab086fe1
Andrew Azores <me@andrewazor.es>
2025-01-24 20:37:42 -0500

    feat(plugin): dynamic lookup of ConsolePlugin CR (#75)
    * feat(plugin): dynamic lookup of ConsolePlugin CR
    
    * backend needs permission to query services, not Cryostat CRs
    
    * env var cleanup, load k8s client cfg from cluster

--- src/openshift/services/ApiService.tsx
+++ src/openshift/services/ApiService.tsx
@@ -13,51 +13,174 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { from, of, Observable } from 'rxjs';
+import { from, of, Observable, ReplaySubject, first, map } from 'rxjs';
 import { catchError, concatMap } from 'rxjs/operators';
-import { consoleFetch } from '@openshift-console/dynamic-plugin-sdk';
+import { consoleFetch, k8sGet, K8sModel, K8sVerb } from '@openshift-console/dynamic-plugin-sdk';
+
+const CONSOLE_PLUGIN_MODEL: K8sModel = {
+  label: 'ConsolePlugin',
+  labelKey: 'public~ConsolePlugin',
+  apiVersion: 'v1',
+  apiGroup: 'console.openshift.io',
+  plural: 'consoleplugins',
+  abbr: 'CP',
+  namespaced: false,
+  kind: 'ConsolePlugin',
+  id: 'consoleplugin',
+  labelPlural: 'ConsolePlugins',
+  labelPluralKey: 'public~ConsolePlugins',
+  crd: true,
+  verbs: ['delete', 'deletecollection', 'get', 'list', 'patch', 'create', 'update', 'watch'] as K8sVerb[],
+};
+
+const PLUGIN_NAME = 'cryostat-plugin'; // this should match the consolePlugin.name in package.json
+
+interface ConsolePluginCustomResource {
+  apiVersion: string;
+  kind: string;
+  metadata: {
+    name: string;
+    namespace: string;
+    labels?: Map<string, string>;
+  };
+  spec: {
+    displayName: string;
+    backend: {
+      type: string;
+      service: {
+        name: string;
+        namespace: string;
+        port: number;
+        basePath: string;
+      };
+    };
+    proxy: {
+      alias: string;
+      authorization: string;
+      endpoint: {
+        type: string;
+        service: {
+          name: string;
+          namespace: string;
+          port: number;
+        };
+      };
+    }[];
+  };
+}
+
+interface ConsolePluginInstance {
+  pluginName: string;
+  proxyAlias: string;
+  proxyNamespace: string;
+}
 
 export class ApiService {
+  constructor(private readonly _pluginInstance = new ReplaySubject<ConsolePluginInstance>()) {
+    this._pluginInstance.subscribe((cryostatConsolePlugin) => console.debug({ pluginInstance: cryostatConsolePlugin }));
+    from(k8sGet({ model: CONSOLE_PLUGIN_MODEL, name: PLUGIN_NAME }))
+      .pipe(
+        first(),
+        map((v) => {
+          if (!v) {
+            throw new Error();
+          }
+          if (v['items']) {
+            return v['items'];
+          }
+          return v;
+        }),
+        map((v) => {
+          if (Array.isArray(v)) {
+            if (v.length !== 1) {
+              console.warn(`Expected to find one ConsolePlugin named ${PLUGIN_NAME}, found: ${v.length}`);
+            }
+            return v[0];
+          }
+          return v;
+        }),
+        catchError((err) => {
+          console.error(err);
+          const defaultCr: ConsolePluginCustomResource = {
+            metadata: {
+              name: PLUGIN_NAME,
+            },
+            spec: {
+              proxy: [
+                {
+                  alias: `${PLUGIN_NAME}-proxy`,
+                  endpoint: {
+                    service: {
+                      namespace: `plugin--${PLUGIN_NAME}`,
+                    },
+                  },
+                },
+              ],
+            },
+          } as ConsolePluginCustomResource;
+          return of(defaultCr);
+        }),
+      )
+      .subscribe((pluginInstance: ConsolePluginCustomResource) => {
+        console.debug({ pluginInstance });
+        this._pluginInstance.next({
+          proxyAlias: pluginInstance.spec.proxy[0].alias,
+          pluginName: pluginInstance.metadata.name,
+          proxyNamespace: pluginInstance.spec.proxy[0].endpoint.service.namespace,
+        });
+      });
+  }
+
   status(): Observable<string> {
-    const url = this.proxyUrl('health');
-    return from(
-      consoleFetch(url.toString(), {
-        method: 'GET',
-        redirect: 'follow',
-      }),
-    ).pipe(
-      concatMap((resp: Response) => (resp.ok ? resp.text() : of(resp.statusText))),
-      catchError((err) => {
-        console.error(err);
-        return JSON.stringify(err);
-      }),
-    );
+    return this.proxyUrl('health')
+      .pipe(
+        concatMap((url) =>
+          from(
+            consoleFetch(url.toString(), {
+              method: 'GET',
+              redirect: 'follow',
+            }),
+          ),
+        ),
+      )
+      .pipe(
+        concatMap((resp: Response) => (resp.ok ? resp.text() : of(resp.statusText))),
+        catchError((err) => {
+          console.error(err);
+          return JSON.stringify(err);
+        }),
+      );
   }
 
   cryostat(ns: string, name: string, method: string, requestPath: string, _body?: object): Observable<string> {
-    const url = this.proxyUrl(`upstream/${requestPath}`);
-    return from(
-      consoleFetch(url.toString(), {
-        method,
-        redirect: 'follow',
-        headers: {
-          'CRYOSTAT-SVC-NS': ns,
-          'CRYOSTAT-SVC-NAME': name,
-        },
-      }),
-    ).pipe(
-      concatMap((resp: Response) => (resp.ok ? resp.text() : of(resp.statusText))),
-      catchError((err) => {
-        console.error(err);
-        return JSON.stringify(err);
-      }),
-    );
+    return this.proxyUrl(`upstream/${requestPath}`)
+      .pipe(
+        concatMap((url) =>
+          from(
+            consoleFetch(url, {
+              method,
+              redirect: 'follow',
+              headers: {
+                'CRYOSTAT-SVC-NS': ns,
+                'CRYOSTAT-SVC-NAME': name,
+              },
+            }),
+          ),
+        ),
+      )
+      .pipe(
+        concatMap((resp: Response) => (resp.ok ? resp.text() : of(resp.statusText))),
+        catchError((err) => {
+          console.error(err);
+          return JSON.stringify(err);
+        }),
+      );
   }
 
-  private proxyUrl(requestPath: string): string {
-    const pluginName = 'cryostat-plugin'; // this must match the consolePlugin.name in package.json
-    const proxyAlias = 'cryostat-plugin-proxy'; // this must match the .spec.proxy.alias in the ConsolePlugin CR
-    const url = `/api/proxy/plugin/${pluginName}/${proxyAlias}/${requestPath}`;
-    return url;
+  private proxyUrl(requestPath: string): Observable<string> {
+    return this._pluginInstance.pipe(
+      first(),
+      map((instance) => `/api/proxy/plugin/${instance.pluginName}/${instance.proxyAlias}/${requestPath}`),
+    );
   }
 }
