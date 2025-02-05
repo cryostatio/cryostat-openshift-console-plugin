03310d204e3be9efbf52ee93c4a123d75efdf78d
aptmac <almacdon@redhat.com>
2025-02-05 17:18:55 -0500

    fix(css): apply missing stylesheets to Topology and Recordings (#126)
    (cherry picked from commit 84cfda5ffb03a62e9c992aed8d47a14d7c84722c)

--- src/openshift/pages/RecordingsPage.tsx
+++ src/openshift/pages/RecordingsPage.tsx
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import '@patternfly/patternfly/components/OverflowMenu/overflow-menu.css';
 import React from 'react';
 import { CryostatContainer } from '@console-plugin/components/CryostatContainer';
 import Recordings from '@app/Recordings/Recordings';
