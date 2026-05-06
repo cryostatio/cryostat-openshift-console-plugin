fb9d30e43a436ff1f719b5df02e630fc08104f9f
Andrew Azores <aazores@ibm.com>
2026-05-06 11:12:25 -0400

    chore(release): prepare for Cryostat 4.3.0-dev

--- console-plugin-metadata.ts
+++ console-plugin-metadata.ts
@@ -17,7 +17,7 @@ import { ConsolePluginBuildMetadata } from '@openshift-console/dynamic-plugin-sd
 
 export const pluginMetadata: ConsolePluginBuildMetadata = {
   name: 'cryostat-plugin',
-  version: '4.2.0-dev',
+  version: '4.3.0-dev',
   displayName: 'Cryostat OpenShift Console Plugin',
   description: 'OpenShift Console plugin for Cryostat',
   exposedModules: {
